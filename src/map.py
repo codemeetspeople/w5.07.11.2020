2f4eaf387f56611b1fb25c99fd31e826d79e7f0f
caiman <me@caiman.dev>
2020-11-07 15:00:15 +0200

    [master] Map

---
+++ src/map.py
@@ -0,0 +1,31 @@
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def increment(x):
+    return x + 1
+
+def power(x):
+    return x ** 2
+
+def sequence_handler(func, sequence):
+    result = []
+
+    for elem in sequence:
+        result.append(func(elem))
+
+    return result
+
+
+print(array)
+print(sequence_handler(increment, array))
+print(sequence_handler(power, array))
+print('===============')
+
+print(array)
+print(sequence_handler(lambda x: x + 1, array))
+print(sequence_handler(lambda x: x ** 2, array))
+print('===============')
+
+print(array)
+print(list(map(lambda x: x + 1, array)))
+print(list(map(lambda x: x ** 2, array)))
+print('===============')
