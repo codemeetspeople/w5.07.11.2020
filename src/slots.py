8e4505e81a88f15141c4f4ec8458a93ad57ae572
caiman <me@caiman.dev>
2020-11-08 15:16:12 +0200

    [master] __slots__ vs __dict__

--- src/slots.py
+++ src/slots.py
@@ -2,17 +2,25 @@ from pympler.asizeof import asizeof
 
 
 class A:
-    def __init__(self, x=0, y=0):
+    def __init__(self, x=0, y=0, z=0):
         self.x = x
         self.y = y
+        self.z = z
+
+    def __str__(self):
+        return f'({self.x}, {self.y}, {self.z})'
 
 
 class B:
-    __slots__ = ['x', 'y']
+    __slots__ = ['x', 'y', 'z', 'point']
 
-    def __init__(self, x=0, y=0):
+    def __init__(self, x=0, y=0, z=0):
         self.x = x
         self.y = y
+        self.z = z
+
+    def __str__(self):
+        return f'({self.x}, {self.y}, {self.z})'
 
 
 if __name__ == '__main__':
